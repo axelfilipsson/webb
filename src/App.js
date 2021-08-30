82416a1a181bb227cc63e6780f33928e15d67497
Axel Filipsson <filipsson960409@gmail.com>
2021-08-30 16:47:03 +0200

    added construction text

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import './App.css';
+
+import styled from 'styled-components';
+
+const Root = styled.div`
+  text-align: center; 
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-        <p>
-         Välkommen!
-        </p>
-       
-      </header>
-    </div>
+    <Root>
+         <h2> Välkommen! </h2>
+      <p>
+        Sidan är under uppbyggnad..
+      </p>
+   
+    </Root>
   );
 }
 
